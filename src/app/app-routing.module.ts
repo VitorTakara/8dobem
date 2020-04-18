cfda86a1df773bee85ca1d162b2558c119318732
Takara <vtakaraa@gmail.com>
2020-04-18 16:13:32 -0300

    Incrementos na Home e Criação do Footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { GuideDetailComponent } from './guide-detail/guide-detail.component';
 
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'guia/:id', component: GuideDetailComponent },
   { path: '**', redirectTo: 'home' }
 ];
 
