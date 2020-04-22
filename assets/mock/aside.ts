f3085fafefb976b9e81dad3329ba9c229a704215
Takara <vtakaraa@gmail.com>
2020-04-22 09:28:35 -0300

    Auto-generated commit

---
+++ assets/mock/aside.ts
@@ -0,0 +1,13 @@
+export const ASIDEITEMS = [{
+    thumb: "http://placekitten.com/1000/1000",
+    title: "É FAKE NEWS!",
+    subtitle: "NÃO ACREDITE EM TUDO QUE VOCÊ RECEBE. SAIBA AGORA O QUE É VERDADEIRO!",
+    link: "#"
+  },
+  {
+    thumb: "http://placekitten.com/700/700",
+    title: "CONFIRA O NÚMERO DE INFECTADOS!",
+    subtitle: "NO BRASIL E NO MUNDO!",
+    link: "#"
+  }
+]
