5b854769c94e57109e46e33abb7a5ba490df7bd6
Takara <vtakaraa@gmail.com>
2020-05-10 22:13:26 -0300

    MVP

--- assets/mock/aside.ts
+++ assets/mock/aside.ts
@@ -3,13 +3,13 @@ export const ASIDEITEMS = [{
     title: "É FAKE NEWS!",
     subtitle: "NÃO ACREDITE EM TUDO QUE VOCÊ RECEBE. SAIBA AGORA O QUE É VERDADEIRO!",
     alt: "Imagem ilustativa de uma pessoa interagindo com seu celular onde nele mostra notícias referentes ao vírus",
-    link: "#"
+    route: "/fakenews"
   },
   {
     thumb: "../../assets/images/thumb/covid_infectados_mortes.jpg",
     title: "CONFIRA O NÚMERO DE INFECTADOS!",
     alt: "Imagem ilustrativa de diversos caixões sendo tombados",
     subtitle: "NO BRASIL E NO MUNDO!",
-    link: "#"
+    route: "/mapa"
   }
 ]
