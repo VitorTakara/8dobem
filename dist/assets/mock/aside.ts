2ac2a5395594bebee71dd06f6124350b167dcab5
Takara <vtakaraa@gmail.com>
2020-05-11 08:06:38 -0300

    Inserido google analitycs git push

---
+++ dist/assets/mock/aside.ts
@@ -0,0 +1,15 @@
+export const ASIDEITEMS = [{
+    thumb: "../../assets/images/thumb/covid_fakenews_verao.jpg",
+    title: "É FAKE NEWS!",
+    subtitle: "NÃO ACREDITE EM TUDO QUE VOCÊ RECEBE. SAIBA AGORA O QUE É VERDADEIRO!",
+    alt: "Imagem ilustativa de uma pessoa interagindo com seu celular onde nele mostra notícias referentes ao vírus",
+    route: "/fakenews"
+  },
+  {
+    thumb: "../../assets/images/thumb/covid_infectados_mortes.jpg",
+    title: "CONFIRA O NÚMERO DE INFECTADOS!",
+    alt: "Imagem ilustrativa de diversos caixões sendo tombados",
+    subtitle: "NO BRASIL E NO MUNDO!",
+    route: "/mapa"
+  }
+]
---
+++ dist/assets/mock/guides.ts
@@ -0,0 +1,49 @@
+export const GUIDES = [{
+  id: "1",
+  title: "1. O que é a COVID-19?",
+  description: "Saiba o que é COVID-19, de onde veio e como chegou até o Brasil",
+  alt: "Imagem ilustrativa de uma pessoa com equipamento de proteção segurando um sugador de vírus",
+  thumb: "../../assets/images/thumb/1_thumb_o_que_e_covid.jpg"
+}, {
+  title: "2. Como a COVID-19 é transmitida?",
+  description: "O SARS-cov-2 é um virus respiratório, descubra como acontece a transmissão.",
+  id: "2",
+  alt: "Imagem ilustrativa de pessoas aglomeradas em um espaço vúlnerável",
+  thumb: "../../assets/images/thumb/2_thumb_transmissao.jpg"
+}, {
+  title: "3. O que são os Grupos de Risco?",
+  description: "As pessoas mais frágeis devem ser protegidas, saiba quem são elas.",
+  id: "3",
+  alt: "Imagem de um senhor sentado tomando seu vinho dentro de um restaurante ou bar",
+  thumb: "../../assets/images/thumb/3_thumb_grupo_risco.jpg"
+}, {
+  title: "4. Como posso me proteger e proteger minha família do novo Coronavírus?",
+  description: "Aprenda medidas fundamentais para proteger a si e todos que você ama!",
+  id: "4",
+  alt: "Imagem de uma escultura utilizando um máscara de proteção",
+  thumb: "../../assets/images/thumb/4_thumb_protecao.jpg"
+}, {
+  title: "5. Qual a importância do distanciamento social?",
+  description: "O distanciamento social é a melhor forma de combater esse inimigo, descubra o motivo!",
+  id: "5",
+  alt: "Imagem de 4 pessoas atravessando a rua na faixa de pedestres, porém a imagem foi cortada e afastada em cada pessoa para demonstrar o distânciamento",
+  thumb: "../../assets/images/thumb/5_thumb_distanciamento_social.jpg"
+}, {
+  title: "6. Quais são os sintomas da COVID-19?",
+  description: "Os sintomas são os sinais perceptíveis que uma doença pode causar. Saiba quais são os sintomas da COVID-19.",
+  id: "6",
+  alt: "Imagem de um termômetro",
+  thumb: "../../assets/images/thumb/6_thumb_sintomas.jpg"
+}, {
+  title: "7. A Covid-19 tem cura?",
+  description: "O novo coronavírus é perigoso, porém não é uma sentença.",
+  id: "7",
+  alt: "Imagem ilustrativa",
+  thumb: "../../assets/images/thumb/7_thumb_cura.jpg"
+}, {
+  title: "8. O que mais preciso saber sobre a COVID-19?",
+  description: "Mantenha uma vida saudável, durante a quarentena. Se cuide!",
+  id: "8",
+  alt: "Imagem ilustrativa",
+  thumb: "../../assets/images/thumb/8_thumb_saber.jpg"
+}]
