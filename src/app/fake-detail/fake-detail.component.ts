444d9b699039970f96ae871ae0e1669f5c6ecea7
Takara <vtakaraa@gmail.com>
2020-05-10 21:11:46 -0300

    Finalização do MVP

--- src/app/fake-detail/fake-detail.component.ts
+++ src/app/fake-detail/fake-detail.component.ts
@@ -12,7 +12,22 @@ export class FakeDetailComponent implements OnInit {
 
   public guide = FAKE_DETAILS[0];
 
-  ngOnInit(): void {
+  shareGuide(type){
+    if(type === "facebook"){
+      let w = 630;
+      let h = 360;
+
+      let left = screen.width/2 - 630/2;
+      let top = screen.height/2 - 360/2;
+
+      window.open(`
+      http://www.facebook.com/sharer.php?u=https://www.guiacovid19.com.br/%23/home`,
+      'Compartilhar no facebook', 'toolbar=no, location=no, directories=no, status=no, ' + 'menubar=no, scrollbars=yes, resizable=no, copyhistory=no, width='+w+ ', height=' + h + ', top=' + top + ', left=' + left);
+    } else {
+      window.open(`https://wa.me/?text=Olha só que matéria legal que eu vi no portal do Guia COVID-19 sobre ${this.guide.title}: https://www.guiacovid19.com.br/%23/guia/${this.GUIDE_ID}`)
+    }
   }
+
+  ngOnInit(): void {}
 
 }
