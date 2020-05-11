24c617d184cd6f294258ea137c0a9905c3982990
Takara <vtakaraa@gmail.com>
2020-05-10 21:13:05 -0300

    Finalização do MVP

--- src/app/fake-detail/fake-detail.component.ts
+++ src/app/fake-detail/fake-detail.component.ts
@@ -24,7 +24,7 @@ export class FakeDetailComponent implements OnInit {
       http://www.facebook.com/sharer.php?u=https://www.guiacovid19.com.br/%23/home`,
       'Compartilhar no facebook', 'toolbar=no, location=no, directories=no, status=no, ' + 'menubar=no, scrollbars=yes, resizable=no, copyhistory=no, width='+w+ ', height=' + h + ', top=' + top + ', left=' + left);
     } else {
-      window.open(`https://wa.me/?text=Olha só que matéria legal que eu vi no portal do Guia COVID-19 sobre ${this.guide.title}: https://www.guiacovid19.com.br/%23/guia/${this.GUIDE_ID}`)
+      window.open(`https://wa.me/?text=Olha só que matéria legal que eu vi no portal do Guia COVID-19 sobre ${this.guide.title}: https://www.guiacovid19.com.br/%23/fakenews/`)
     }
   }
 
