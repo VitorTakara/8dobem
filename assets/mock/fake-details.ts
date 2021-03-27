1c539d15a19605c3b313e910d3e1a2c13f60c3c9
Takara <vtakaraa@gmail.com>
2021-03-27 09:19:54 -0300

    Auto-generated commit

---
+++ assets/mock/fake-details.ts
@@ -0,0 +1,28 @@
+export const FAKE_DETAILS = [{
+    id: 1,
+    title: "O VERÃO PODE AJUDAR A CONTER A TRANSMISSÃO DO VÍRUS?",
+    subtitle: "Saiba se um país com o clima mais quente interefere na propagação do vírus",
+    date: "Mai 11, 2020",
+    readTime: 2,
+    audio: "",
+    shareButtons: [{
+      icon: "icon-facebook",
+      type: "facebook"
+    }, {
+      icon: "icon-whatsapp",
+      type: "whatsapp"
+    }],
+    thumb: {
+      img: "../../assets/images/banners/fake_news_verao.jpg",
+      alt: "Imagem contendo imagens rementendo o verão. Um abacaxi na piscina, uma bóia de piscina no formato de um flamingo e uma escada de piscina"
+    },
+    paragraphs: [{
+        paragraph: "Os dados da Organização Mundial da Saúde (OMS) demonstram que a pandemia cursa tanto em países com clima mais frio (caso da Itália e da China) como em locais mais quentes, a exemplo de regiões brasileiras e australianas."
+      },
+      {
+        paragraph: "O que se sabe, pela experiência de epidemias de infecções respiratórias anteriores, é que os vírus respiratórios se aproveitam de duas situações: queda na imunidade e aglomeração de pessoas. Fatores comuns com a chegada do inverno"
+      }
+    ],
+    references: ["https://saude.abril.com.br/medicina/como-o-coronavirus-e-transmitido-e-por-quanto-tempo-ele-resiste-por-ai/"]
+  }
+]
